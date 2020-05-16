8196f7086c3f20375aad0a4b4ad92d88481b6b12
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-05-16 11:30:20 -0300

    Remove unnecessary route and template - Disable registration route

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::resource('contacts', 'ContactController');
 
 Route::resource('data', 'DataController');
 
 Route::resource('tags', 'TagController');
 
+Auth::routes(['register' => false]);
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
