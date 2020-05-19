782222699f4df2fc664b206a40cd55e0492a6b69
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-05-19 08:56:44 -0300

    List all contacts

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class ContactController extends Controller
 {
@@ -15,6 +16,8 @@ class ContactController extends Controller
     public function index()
     {
         $contacts = Contact::all();
+
+        return View::make('contacts', compact('contacts'));
     }
 
     /**
