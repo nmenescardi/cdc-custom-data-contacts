9ce2de3718e28ae382e041afc0dff4dec5d6ddc2
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-05-23 11:58:44 -0300

    Add barebone cards into edit contact page

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -3,33 +3,68 @@
 @section('title', 'Edit Contact')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-10 cdc-table__main-col">
-            <div class="card">
-                <div class="card-header">Edit Contact</div>
+<div class="contact-edit cdc-table">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 cdc-table__main-col">
+                <div class="card">
+                    <div class="card-header">Edit Contact</div>
 
-                <div class="card-body">
-                    <form action="{{ route('contacts.update', ['contact' => $contact]) }}" method="POST">
-                        @method('PATCH')
-                        <div class="form-group">
-                            <label class="control-label" for="name">Name: </label>
-                            <input type="text" name="name" id="name" placeholder="Name"
-                                value="{{ old('name') ?? $contact->name }}" required
-                                class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}">
+                    <div class="card-body">
+                        <form action="{{ route('contacts.update', ['contact' => $contact]) }}" method="POST">
+                            @method('PATCH')
+                            <div class="form-group">
+                                <label class="control-label" for="name">Name: </label>
+                                <input type="text" name="name" id="name" placeholder="Name"
+                                    value="{{ old('name') ?? $contact->name }}" required
+                                    class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}">
 
-                            @if( $errors->has('name') )
-                            <div><span class="text-danger">{{ $errors->first('name') }}</span></div>
-                            @endif
-                        </div>
+                                @if( $errors->has('name') )
+                                <div><span class="text-danger">{{ $errors->first('name') }}</span></div>
+                                @endif
+                            </div>
 
-                        <button type="submit" class="btn btn-primary">Save Contact</button>
+                            <button type="submit" class="btn btn-primary">Save Contact</button>
 
-                        @csrf
-                    </form>
+                            @csrf
+                        </form>
+                    </div>
+
                 </div>
-
             </div>
+
+            <div class="col-md-6 cdc-table__main-col">
+                <div class="card">
+                    <div class="card-header">Tags</div>
+
+                    <div class="card-body">
+                        <div class="tags">
+                            @foreach ($contact->tags as $tag)
+                            <span class="tag badge badge-pill badge-primary">{{$tag->name}}</span>
+                            @endforeach
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-12 cdc-table__main-col mt-5">
+                <div class="card">
+                    <div class="card-header">Records</div>
+
+                    <div class="card-body">
+                        <div class="records">
+                            @foreach ($contact->records as $record)
+                            <span class="record">{{$record->title}}</span>
+                            <span class="record">{{-- {{$record->description}} --}}</span>
+                            <br />
+                            @endforeach
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
