a1a0f322db768e40a0d505222b1e08395dba8b4a
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-05-23 09:47:55 -0300

    Prettify

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -10,14 +10,14 @@
                 <div class="card-header">Add Contact</div>
 
                 <div class="card-body">
-                    <form action="{{route('contacts.store')}}" method="POST">
+                    <form action="{{ route('contacts.store') }}" method="POST">
                         <div class="form-group">
                             <label class="control-label" for="name">Name: </label>
-                            <input type="text" name="name" id="name" placeholder="Name" value="{{old('name')}}" required
-                                class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}">
+                            <input type="text" name="name" id="name" placeholder="Name" value="{{ old('name') }}"
+                                required class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}">
 
-                            @if($errors->has('name'))
-                            <div><span class="text-danger">{{ $errors->first('name')}}</span></div>
+                            @if( $errors->has('name') )
+                            <div><span class="text-danger">{{ $errors->first('name') }}</span></div>
                             @endif
                         </div>
 
