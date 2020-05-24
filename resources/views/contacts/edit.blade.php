e48b63a411b321b07aea07928c0e2e7217098612
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-05-24 07:00:09 -0300

    Add link into Contact's tags

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -40,7 +40,11 @@
                     <div class="card-body">
                         <div class="tags">
                             @foreach ($contact->tags as $tag)
-                            <span class="tag badge badge-pill badge-primary">{{$tag->name}}</span>
+                            <span class="tag badge badge-pill badge-primary">
+                                <a href="{{ route('tags.edit', ['tag' => $tag] ) }}">
+                                    {{$tag->name}}
+                                </a>
+                            </span>
                             @endforeach
                         </div>
                     </div>
