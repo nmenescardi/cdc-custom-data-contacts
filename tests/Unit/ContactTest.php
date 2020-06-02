402cffe97fc64881153a3062add5646e297bde0b
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-02 03:50:02 -0300

    Add ContactTest - Test title not nullable

---
+++ tests/Unit/ContactTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Contact;
+use Exception;
+use Illuminate\Database\QueryException;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ContactTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_title_cannot_be_nullable()
+    {
+        $this->expectException(QueryException::class);
+
+        Contact::firstOrCreate([]);
+
+        $this->assertCount(0, Contact::all());
+    }
+}
