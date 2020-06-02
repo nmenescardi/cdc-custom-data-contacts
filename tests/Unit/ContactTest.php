04c909a0908deb418e5eef3f8acc8e822093227d
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-02 09:31:49 -0300

    add unit test add_new_tags_to_a_contact

--- tests/Unit/ContactTest.php
+++ tests/Unit/ContactTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Contact;
+use App\Tag;
 use Exception;
 use Illuminate\Database\QueryException;
 use Tests\TestCase;
@@ -23,7 +24,26 @@ class ContactTest extends TestCase
         $this->assertCount(0, Contact::all());
     }
 
-    //TODO: test new method on Contact model to sync tags
+    /** @test */
+    public function add_new_tags_to_a_contact()
+    {
+        $tagId1 = factory(Tag::class)->create()->id;
+        $tagId2 = factory(Tag::class)->create()->id;
+        $tagId3 = factory(Tag::class)->create()->id;
+
+        $contact = Contact::firstOrCreate(['name' => 'Carlos']);
+
+        $this->assertCount(1, Contact::all());
+
+        $contact->addTags([$tagId1, $tagId2, $tagId3]);
+
+        $newContactTags = $contact->fresh()->tags;
+
+        $this->assertTrue($newContactTags->contains($tagId1));
+        $this->assertTrue($newContactTags->contains($tagId2));
+        $this->assertTrue($newContactTags->contains($tagId3));
+    }
+
 
     //TODO: test relationships?
 }
