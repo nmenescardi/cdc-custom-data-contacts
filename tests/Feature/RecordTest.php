b885ad1b23a12d8dc851cfa3f84359b96d0c97f0
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-02 11:59:39 -0300

    Add unit test a_record_can_be_added

--- tests/Feature/RecordTest.php
+++ tests/Feature/RecordTest.php
@@ -2,19 +2,57 @@
 
 namespace Tests\Feature;
 
+use App\Contact;
+use App\Record;
+use App\Tag;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RecordTest extends TestCase
 {
+
+    use RefreshDatabase;
+
+    protected $authUser;
+    protected $actAs;
+
+    public function setup(): void
+    {
+        parent::setUp();
+        $this->authUser = factory(User::class)->create();
+        $this->actAs = $this->actingAs($this->authUser, 'web');
+    }
+
+
     /** @test */
     public function a_record_can_be_added()
     {
-        $this->withoutExceptionHandling();
+        $contact = factory(Contact::class, 1)->create()->first();
 
-        //TODO: Title, descr, contact_id, tag_list.
-        //TODO: Place data array in separate method
+        $tagId1 = factory(Tag::class)->create()->id;
+        $tagId2 = factory(Tag::class)->create()->id;
+        $tagId3 = factory(Tag::class)->create()->id;
+
+        $response = $this->actAs->post('/records', [
+            'title'         => 'Something',
+            'description'   => 'Something else',
+            'contact_id'    => $contact->id,
+            'tag_list'      => [
+                $tagId1, $tagId2, $tagId3
+            ]
+        ]);
+
+        $this->assertCount(1, Record::all());
+
+        $contactID = Record::first()->contact->id;
+        $response->assertRedirect(route('contacts.edit', $contactID));
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
     }
 
     //TODO: Title is required
