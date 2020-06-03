8feee0247d6fc9f9201b50f3458f7ad20aaa7632
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-03 10:08:02 -0300

    Add Enum for Tag Colors

---
+++ app/Enums/TagColor.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static DarkBlue()
+ * @method static static Blue()
+ * @method static static Indigo()
+ * @method static static Purple()
+ * @method static static Pink()
+ * @method static static Red()
+ * @method static static Orange()
+ * @method static static Yellow()
+ * @method static static DarkYellow()
+ * @method static static Green()
+ * @method static static Teal()
+ * @method static static Cyan()
+ */
+final class TagColor extends Enum
+{
+    const DarkBlue = 'dark-blue';
+    const Blue = 'blue';
+    const Indigo = 'indigo';
+    const Purple = 'purple';
+    const Pink = 'pink';
+    const Red = 'red';
+    const Orange = 'orange';
+    const Yellow = 'yellow';
+    const DarkYellow = 'dark-yellow';
+    const Green = 'green';
+    const Teal = 'teal';
+    const Cyan = 'cyan';
+}
