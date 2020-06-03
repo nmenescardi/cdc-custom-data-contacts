77e9c40e969cf795892a500122bcd22a71b7293e
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-03 17:24:48 -0300

    Add dynamic color into tag component - Fix cursor pointer issue

--- resources/views/components/tags-list.blade.php
+++ resources/views/components/tags-list.blade.php
@@ -2,7 +2,7 @@
 
 <div class="tags">
     @foreach ($tagList as $tag)
-    <span class="tag badge badge-pill badge-primary">
+    <span class="tag badge badge-pill bg-color-{{$tag->color}}">
         <a href="{{ route('tags.edit', ['tag' => $tag] ) }}">
             {{$tag->name}}
         </a>
