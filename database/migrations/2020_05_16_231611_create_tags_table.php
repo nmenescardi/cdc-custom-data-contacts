92fe1fa5860eae7331d67adf8c2b42f9d5227062
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-03 10:08:22 -0300

    Update Migration with new color type column

--- database/migrations/2020_05_16_231611_create_tags_table.php
+++ database/migrations/2020_05_16_231611_create_tags_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\TagColor;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +17,10 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->enum(
+                'color',
+                TagColor::getValues()
+            )->default(TagColor::DarkBlue);
             $table->timestamps();
         });
     }
