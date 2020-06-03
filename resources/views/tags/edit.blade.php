6118de96cd5aaddb63bea45e0d96051fc9bc92c9
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-03 10:40:41 -0300

    Add color selector

--- resources/views/tags/edit.blade.php
+++ resources/views/tags/edit.blade.php
@@ -25,6 +25,27 @@
                                 @endif
                             </div>
 
+                            <div class="form-group">
+                                <div class="select-colors">
+                                    <label for="colors">Select Color:</label>
+                                    <select name="color" id="color" class="form-control">
+                                        @foreach ($colors as $key => $color)
+                                        <option value="{{$key}}" {{
+                                                (null !== old('color') && old('color') === $key)
+                                                || (null == old('color') && $tag->color === $key)
+                                                ? 'selected' : '' }}>
+                                            {{$color}}
+                                        </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                @if( $errors->has('color') )
+                                <div><span class="text-danger">{{ $errors->first('color') }}</span></div>
+                                @endif
+                            </div>
+
+
                             <button type="submit" class="btn btn-primary">Save Tag</button>
 
                             @csrf
