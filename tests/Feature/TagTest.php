c1a43f0635fe4656327ab36b0affeaf013f053dc
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-03 07:22:36 -0300

    add test_a_single_tag_can_be_added

--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -2,18 +2,34 @@
 
 namespace Tests\Feature;
 
+use App\Tag;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TagTest extends TestCase
 {
-    /** @test */
-    public function a_single_tag_can_be_added()
-    {
-        $this->withoutExceptionHandling();
+    use RefreshDatabase;
 
-        //TODO: name (only one)
+    protected $authUser;
+    protected $actAs;
+
+    public function setup(): void
+    {
+        parent::setUp();
+        $this->authUser = factory(User::class)->create();
+        $this->actAs = $this->actingAs($this->authUser, 'web');
+    }
+
+
+    public function test_a_single_tag_can_be_added()
+    {
+        $tag_list = ['SomeTag'];
+
+        $this->actAs->post(route('tags.store'), ['tag_list' => $tag_list]);
+
+        $this->assertCount(1, Tag::all());
     }
 
     //TODO: store multiple tags.
