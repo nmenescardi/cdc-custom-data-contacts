60dc81f31b1d802b3dad370be4c8a9880d48a2d4
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-04 07:53:02 -0300

    Add random color value into factory

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -2,11 +2,13 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Enums\TagColor;
 use App\Tag;
 use Faker\Generator as Faker;
 
 $factory->define(Tag::class, function (Faker $faker) {
     return [
-        'name' => $faker->unique()->word()
+        'name'  => $faker->unique()->word(),
+        'color' => TagColor::getRandomValue()
     ];
 });
