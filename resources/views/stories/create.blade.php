24e13cd114586cb3320756f2fedb0b8de60d067f
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-05 10:28:13 -0300

    Add template for creating Stories

---
+++ resources/views/stories/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', 'Add Story')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-10 cdc-table__main-col">
+            <div class="card">
+
+                <x-table-header title="Add Story" />
+
+                <div class="card-body">
+                    <form action="{{ route('stories.store') }}" method="POST">
+
+                        <x-stories-form :story="$story" :allTags="$allTags" buttonLabel="Add Story" />
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
