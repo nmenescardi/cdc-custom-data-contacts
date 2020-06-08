c6647ccd136e1eab21d939013764daabc2662217
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-08 08:28:10 -0300

    Add command to expire stories

---
+++ app/Console/Commands/ExpireStories.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Story;
+use Illuminate\Console\Command;
+
+class ExpireStories extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cdc:expire-stories';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'It expires stories by resting one day in its "days_to_expire". If a story reaches zero "days_to_expire" -> the task gets deleted.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $stories = Story::all();
+
+        $stories->each(function (Story $story) {
+
+            if (--$story->days_to_expire > 0)
+                $story->save();
+            else
+                $story->delete();
+        });
+    }
+}
